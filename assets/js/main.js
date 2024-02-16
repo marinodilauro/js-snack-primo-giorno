25b8cd45ed63aaa16645c189e43e90180edc62ab
marinodilauro <m.dilauro90@gmail.com>
2024-02-16 16:42:24 +0100

    Add exercise 2/C

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,21 @@ console.log(pi);
 
 /*
 Esercizio 2/B
-Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
-*/
+Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+
 pi = 3.1416;
 console.log(pi);
+
+Le variabili di tipo const non posso essere ri-assegnate
+*/
+
+/*
+Esercizio 2/C
+- Dichiara una variabile chiamata radius e assegnale il valore 8.
+- Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+- Stampa il valore della variabile circle in console.
+*/
+
+const radius = 8;
+const circle = radius * 2 * pi;
+console.log(circle);
