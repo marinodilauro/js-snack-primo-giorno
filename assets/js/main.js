3e726afac425569bc3bdecfe1602d94c55595aee
marinodilauro <m.dilauro90@gmail.com>
2024-02-16 16:35:17 +0100

    Add exercise 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,12 @@ Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valo
 
 const number = 100;
 console.log(number);
+
+
+/*
+Esercizio 2
+Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+*/
+
+const pi = 3.14;
+console.log(pi);
